657a53d303bc5f02ada66604cae4eadda1cc0806
Imanuel94 <imanuelreborn@gmail.com>
2016-05-25 13:40:43 +0700

    Add files via upload

---
+++ MataKuliah.java
@@ -0,0 +1,68 @@
+package bean;
+
+public class MataKuliah {
+
+    private String kodeMakul;
+    private String namaMakul;
+    private String waktu;
+    private String sks;
+    private String dosen;
+    private String tarif;
+    private String ruang;
+
+    public String getKodeMakul() {
+        return kodeMakul;
+    }
+
+    public void setKodeMakul(String kodeMakul) {
+        this.kodeMakul = kodeMakul;
+    }
+
+    public String getNamaMakul() {
+        return namaMakul;
+    }
+
+    public void setNamaMakul(String namaMakul) {
+        this.namaMakul = namaMakul;
+    }
+
+    public String getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(String waktu) {
+        this.waktu = waktu;
+    }
+
+    public String getSks() {
+        return sks;
+    }
+
+    public void setSks(String sks) {
+        this.sks = sks;
+    }
+
+    public String getDosen() {
+        return dosen;
+    }
+
+    public void setDosen(String dosen) {
+        this.dosen = dosen;
+    }
+
+    public String getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(String tarif) {
+        this.tarif = tarif;
+    }
+
+    public String getRuang() {
+        return ruang;
+    }
+
+    public void setRuang(String ruang) {
+        this.ruang = ruang;
+    }
+}
